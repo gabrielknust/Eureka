f52fe9256a024e39c2d423417396a9268afe6338
Sabrina Sampaio <47161964+Sabrina-Btt@users.noreply.github.com>
2019-11-08 12:54:12 -0300

    Modifiquei construtor

--- classes/Cargo.php
+++ classes/Cargo.php
@@ -5,23 +5,26 @@
 	    private $nome;
 	    public function __construct($idCargo,$nome)
 	    {
-	    	public function getIdCargo()
-	    	{
-	    		return $this->idCargo;
-	    	}
-	    	public function setIdCargo($idCargo)
-	    	{
-	    		$this->idCargo = $idCargo;
-	    	}
-	    	public function getNome()
-	    	{
-	    		return $this->nome;
-	    	}
-	    	public function setNome($nome)
-	    	{
-	    		$this->nome = $nome;
-	    	}
+            $this->idCargo=$idCargo;
+            $this->nome=$nome;
+	    }	
+	    public function getIdCargo()
+	    {
+	    	return $this->idCargo;
 	    }
+	    public function setIdCargo($idCargo)
+	    {
+	    	$this->idCargo = $idCargo;
+	   	}
+	    public function getNome()
+	    {
+	    	return $this->nome;
+	    }
+	    public function setNome($nome)
+	    {
+	    	$this->nome = $nome;
+	    }
+	   
     }
     
 ?>  
--- classes/Plantao.php
+++ classes/Plantao.php
@@ -6,31 +6,35 @@
 	    private $dia;
 	    public function __construct($idPlantao,$horario)
 	    {
-	    	public function getIdPlantao()
-	    	{
-	    		return $this->idPlantao;
-	    	}
-	    	public function setIdPlantao($idPlantao)
-	    	{
-	    		$this->idPlantao = $idPlantao;
-	    	}
-	    	public function getHorario()
-	    	{
-	    		return $this->horario;
-	    	}
-	    	public function setHorario($horario)
-	    	{
-	    		$this->horario = $horario;
-	    	}
-	    	public function getDia()
-	    	{
-	    		return $this->dia;
-	    	}
-	    	public function setIdPlantao($dia)
-	    	{
-	    		$this->dia = $dia;
-	    	}
+            $this->idPlantao=$idPlantao;
+            $this->horario=$horario;
+            $this->dia=$dia;
+	    }	
+	    public function getIdPlantao()
+	    {
+	    	return $this->idPlantao;
 	    }
+	    public function setIdPlantao($idPlantao)
+	    {
+	    	$this->idPlantao = $idPlantao;
+	    }
+	    public function getHorario()
+	    {
+	    	return $this->horario;
+	    }
+	    public function setHorario($horario)
+	    {
+	    	$this->horario = $horario;
+	    }
+	    public function getDia()
+	    {
+	    	return $this->dia;
+	    }
+	    public function setIdPlantao($dia)
+	    {
+	    	$this->dia = $dia;
+	    }
+	    
     }
     
 ?>  
