e92ae5233e2a6043173482598c6f3bb12d583311
Sabrina Sampaio <47161964+Sabrina-Btt@users.noreply.github.com>
2019-11-08 12:08:30 -0300

    Adicionei dataNasc

--- classes/Funcionario.php
+++ classes/Funcionario.php
@@ -1,73 +1,79 @@
 <?php
-	private class Funcionario
-	{
-		private $idfuncionario;
+    private class Funcionario
+    {
+        private $idfuncionario;
+        private $cpf;
+        private $senha;
+        private $nome;
+        private $email;
+        private $matricula;
+        private $dataNasc;
+        public function __construct($idfuncionario,$cpf,$senha,$nome,$email,$matricula,$dataNasc)
+        {
+            $this->idfuncionario=$idfuncionario;
+            $this->senha=$senha;
+            $this->nome=$nome;
+            $this->email=$email;
+            $this->matricula=$matricula;
+            $this->dataNasc=$dataNasc
+        }
+        public function getIdFuncionario()
+        {
+            return $this->idfuncionario;
+        }
+        public function setIdFuncionario($idfuncionario)
+        {
+            $this->idfuncionario = $idfuncionario;
+        }
+        public function getCpf()
+        {
+            return $this->cpf;
+        }
+        public function setCpf($cpf)
+        {
+            $this->cpf = $cpf;
+        }
+        public function getSenha()
+        {
+            return $this->senha;
+        }
+        public function setSenha($senha)
+        {
+            $this->senha = $senha;
+        }
+        public function getNome()
+        {
+            return $this->nome;
+        }
+        public function setNome($nome)
+        {
+            $this->nome = $nome;
+        }
+        public function getEmail()
+        {
+            return $this->email;
+        }
+        public function setEmail($email)
+        {
+            $this->email = $email;
+        }
+        public function getMatricula()
+        {
+            return $this->cpf;
+        }
+        public function setMatricula($cpf)
+        {
+            $this->cpf = $cpf;
+        }
+        public function getDataNasc()
+        {
+            return $this->dataNasc;
+        }
+        public function setDataNasc($dataNasc)
+        {
+            $this->dataNasc = $dataNasc;
+        }
+    }
 
-	    private $cpf;
-
-	    private $senha;
-
-	    private $nome;
-
-	    private $email;
-
-	    private $matricula;
-
-	    public function __construct($idfuncionario,$cpf,$senha,$nome,$email,$matricula)
-    	{
-	    	$this->idfuncionario=$idfuncionario;
-	    	$this->senha=$senha;
-	    	$this->nome=$nome;
-	    	$this->email=$email;
-	    	$this->matricula=$matricula;
-    	}
-    	public function getIdFuncionario()
-    	{
-        	return $this->idfuncionario;
-    	}
-    	public function setIdFuncionario($idfuncionario)
-    	{
-        	$this->idfuncionario = $idfuncionario;
-    	}
-    	public function getCpf()
-    	{
-        	return $this->cpf;
-    	}
-    	public function setCpf($cpf)
-    	{
-        	$this->cpf = $cpf;
-    	}
-    	public function getSenha()
-    	{
-        	return $this->senha;
-    	}
-    	public function setSenha($senha)
-    	{
-        	$this->senha = $senha;
-    	}
-    	public function getNome()
-    	{
-        	return $this->nome;
-    	}
-    	public function setNome($nome)
-    	{
-        	$this->nome = $nome;
-    	}
-    	public function getEmail()
-    	{
-        	return $this->email;
-    	}
-    	public function setEmail($email)
-    	{
-        	$this->email = $email;
-    	}
-    	public function getMatricula()
-    	{
-        	return $this->cpf;
-    	}
-    	public function setMatricula($cpf)
-    	{
-        	$this->cpf = $cpf;
-    	}
-	}
+    
 ?>
